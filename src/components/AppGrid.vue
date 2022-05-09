04e5c9813a7d7712300361f798f201492039ba3f
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-05-09 18:26:40 +0200

    vue: AppSelect

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="container">
+        <app-select @mySelect="setSelectText($event)" :albumGenres="genres"/>
         <app-loader v-if="loader"/>
         <div class="row">
-            <div v-for="album in albumList" :key="album.id"  class="col-6 col-md-4 col-lg-2 my-5"> 
+            <div v-for="album in filteredAlbum" :key="album.id"  class="col-6 col-md-4 col-lg-2 my-5"> 
                 <app-card :item="album"/>
             </div>
         </div>
@@ -13,33 +14,56 @@
 import axios from "axios"
 import AppCard from './AppCard.vue'
 import AppLoader from './AppLoader.vue'
+import AppSelect from './AppSelect.vue'
 
 export default {
     name: "AppGrid",
     components: { 
         AppCard,
-        AppLoader 
+        AppLoader,
+        AppSelect 
     },
     data(){
         return{
             albumList: [],
+            searchText:"",
             loader: true,
+            genres: [],
+        }
+    },
+    methods:{
+        setSelectText(txt){
+            this.searchText = txt;
+        }
+    },
+    computed:{
+        filteredAlbum(){
+            if(this.searchText === "") return this.albumList;
+            return this.albumList.filter((el)=> {
+                return el.genre === this.searchText
+            });
+                
         }
     },
     mounted(){
         this.loader = true;
-        setTimeout(()=>{
+        
             axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res)=>{
                 console.log(res.data.response)
                 this.albumList = res.data.response
+                this.albumList.forEach((el)=>{
+                    if(!this.genres.includes(el.genre)){
+                        this.genres.push(el.genre);
+                    }
+                })
                 this.loader = false;
             }).catch((err)=>{
                 console.log(err)
                 this.loader = false;
             })
-        },2000)
+        }
     }
-}
+
 </script>
 
 <style lang="scss" scoped>
@@ -48,6 +72,9 @@ export default {
 .col-lg-2{
     flex: 0 0 18%;
     display: flex;
+}
+.container{
+    padding-top: 50px;
 }
 
 </style>
---
+++ src/components/AppSelect.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="input-group">
+            <select name="genre" id="genre" v-model="selectInput" @change="changeSelect">
+                <option value="">All</option>
+                <option :value="genre" v-for="genre in albumGenres" :key="genre">
+                    {{genre}}
+                </option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppSelect",
+    props:[
+        "albumGenres"
+    ],
+    data(){
+        return{
+            selectInput: "",
+        }
+    },
+    methods:{
+        changeSelect(){
+            this.$emit("mySelect", this.selectInput)
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/vars.scss';
+
+select{
+    height: 61px;
+    width: 20%;
+    font-size: 20px;
+    border-radius: 20px;
+    padding: 7px;
+}
+
+
+</style>
